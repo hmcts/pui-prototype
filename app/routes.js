559402d83630655cda507e26b442d647a9162d71
Adam Silver <adambsilver@gmail.com>
2018-04-11 15:31:23 +0100

    Success message flash

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,16 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
+router.post('/v1/get-new-case', function (req, res) {
+	req.session.success = true;
+	res.redirect('/v1/case');
+})
+
+router.get('/v1/case', function (req, res) {
+	res.render('v1/case', { success: req.session.success })
+	req.session.success = null;
+})
+
+
+
 module.exports = router
