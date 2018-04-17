b2d254afbb7eacc076352f1125cc50d1e8152382
Trevor Saint <hello@trevorsaint.com>
2018-04-17 09:12:54 +0100

    JSON data, cases and routing for Divorce

--- app/routes.js
+++ app/routes.js
@@ -1,66 +1,25 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express');
+const router  = express.Router();
 
 
-// Models
-const userEngine = require("./models/users");
+router.use('/', require('./routes/divorce'));
 
 
-// Route index page
-router.get("/", function (req, res) {
-  res.render("index", {
-		users: userEngine.getUsersEntries()
-	});
-});
-
-
-router.get("/dashboard/:id", function(req, res) {
- 
-	entry = userEngine.getUsersEntry(req.params.id);
-	
-	var cases = entry.cases.map(function(c) {
-
-		var newCase = []; 
-	
-		Object.keys(c).forEach(function (key) {
-
-			if(key === "number") {
-				
-					newCase.push({
-						html: '<a href="/case/' + c.number + '">' + c.number + '</a>'
-					});
-
-				} else {
-
-					newCase.push({ 
-						text: c[key]
-					});
-
-				}
-				
-		});
-
-		return newCase;
-
-	});
-
-  res.render("dashboard", {
-		cases: cases,
-		users: entry
-	});
-	
+// Index
+router.get('/', function (req, res) {
+	res.render('index');
 });
 
 
 // Add your routes here - above the module.exports line
-router.post("/v1/get-new-case", function (req, res) {
+router.post('/v1/get-new-case', function (req, res) {
 	req.session.success = true;
-	res.redirect("/v1/case");
+	res.redirect('/v1/case');
 });
 
 
-router.get("/v1/case", function (req, res) {
-	res.render("v1/case/index", { success: req.session.success });
+router.get('/v1/case', function (req, res) {
+	res.render('v1/case/index', { success: req.session.success });
 	req.session.success = null;
 });
 
