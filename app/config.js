ba0e2e4ed420800e886e94738812908464a606cf
Trevor Saint <hello@trevorsaint.com>
2018-06-18 14:39:43 +0100

    Changed service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Judicial case manager',
+  serviceName: 'Professional case manager',
 
   // Default port that prototype runs on
   port: '3000',
