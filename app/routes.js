4f6ff0f7cc5160e51e542e73e6227278e7aa7b12
Adam Silver <adambsilver@gmail.com>
2018-08-09 08:06:15 +0100

    Updated primary nav and made apply flow placeholder

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+router.get('/signout', function (req, res) {
 	req.session.destroy();
-	res.redirect('/app/signin.html');
+	res.redirect('/');
 });
 
 module.exports = router;
