a0aaf71d7d17807db70a59c3b7ac9d5dae0257c8
Adam Silver <adambsilver@gmail.com>
2018-08-31 08:30:34 +0100

    Started manage account

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage a case',
+  serviceName: 'Manage HMCTS case',
 
   // Default port that prototype runs on
   port: '3000',
