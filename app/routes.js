86af7906d161c97e28d1a6883c1dc58911d8c68b
Pete Williams <pete.williams82@gmail.com>
2020-10-16 15:40:47 +0100

    fixing broken thigns

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,17 @@
 var express = require('express');
-var router 	= express.Router();
-var strPath = 'oct-2020';
+var router  = express.Router();
 
-router.post( '/' + strPath + '/register-organisation/organisation-sra-check', (req, res) => {
-	if (req.body['sra-number'] === '123456') {
-		res.redirect('/' + strPath + '/register-organisation/organisation-sra-error');
-	} else {
-		res.redirect('/' + strPath + '/register-organisation/organisation-address');
-	}
+router.use('/', require('./routes/mvp/manage-organisation'));
+router.use('/', require('./routes/experimental'));
+router.use('/', require('./views/oct-2020/routes'));
+
+router.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+router.get('/sign-out', function (req, res) {
+	req.session.destroy();
+	res.redirect('/');
 });
 
 module.exports = router;
--- app/views/oct-2020/routes.js
+++ app/views/oct-2020/routes.js
@@ -1,6 +1,13 @@
+var express = require('express');
+var router 	= express.Router();
 var strPath = 'oct-2020';
 
-module.exports = function(app)
-{
+router.post( '/' + strPath + '/register-organisation/organisation-sra-check', (req, res) => {
+	if (req.body['sra-number'] === '123456') {
+		res.redirect('/' + strPath + '/register-organisation/organisation-sra-error');
+	} else {
+		res.redirect('/' + strPath + '/register-organisation/organisation-address');
+	}
+});
 
-}
+module.exports = router;
