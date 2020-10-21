8d9c537d3fee68f5849ea00f89115323abd45084
Pete Williams <pete.williams82@gmail.com>
2020-10-21 16:23:23 +0100

    adding ability to view/clear data

--- app/routes.js
+++ app/routes.js
@@ -14,4 +14,14 @@ router.get('/sign-out', function (req, res) {
 	res.redirect('/');
 });
 
+router.get('*/prototype-admin/view-data', function(req, res){
+
+	querystring = '';
+	for ( var key in req.session.data )
+	querystring += key +'=' + req.session.data[key] + '&';
+
+  res.render('prototype-admin/view-data', { data: JSON.stringify( req.session, null, 2), querystring: querystring } )
+});
+
+
 module.exports = router;
